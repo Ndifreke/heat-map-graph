17322bff6488d8dbb4bd9a1f477cf67805fe11f6
ndifreke <ndyekim@gmail.com>
2019-09-25 19:52:18 +0100

    chore(static-page): build static page with dummy data

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import MonthComponent from './components/MonthComponent';
+import DayLabels from './components/DaysLabelComponent'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+  buildYearGraph() {
+    const months = []
+    for (const month in [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]) {
+      months.push(<MonthComponent month={month} />)
+    }
+    return months
+  }
+
+  render() {
+    return (
+      <div style={{ display: "flex" }}>
+        <DayLabels />
+        {this.buildYearGraph()}
+      </div>
+
+    );
+  }
+
 }
+
 
 export default App;
