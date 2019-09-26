6811db10c3c287b5350f32b99040b683c346afed
ndifreke <ndyekim@gmail.com>
2019-09-26 14:25:05 +0100

    switching to approach 2

--- src/helper/transactionHelper.js
+++ src/helper/transactionHelper.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import moment from 'moment'
 
 
@@ -54,39 +53,3 @@ export const filterTransactionByDayOfYear = (day, transactions) => {
     details.transactions = transactions.slice(offset)
     return details;
 }
-
-
-=======
-
-
-const months = {
-    0: "january",
-    1: "febuary",
-    2: "march",
-    3: "april",
-    4: "may",
-    5: "june",
-    6: "july",
-    7: "august",
-    8: "september",
-    9: "october",
-    10: "november",
-    11: "december"
-}
-
-const sortTransactionByMonth = (transactionData) => {
-    const sortByMonth = {}
-    transactionData.forEach(transaction => {
-        const monthNumber = new Date(transaction.date).getMonth()
-        const monthName = months[monthNumber]
-        if (monthName in sortByMonth) {
-            sortByMonth[monthName].push(transaction)
-        } else {
-            sortByMonth[monthName] = [transaction]
-        }
-    })
-    return sortByMonth
-}
-
-export default { sortTransactionByMonth, months }
->>>>>>> kkee
