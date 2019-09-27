399e6a2e3032ffe69697cfe582ebdf65bc28ba41
ndifreke <ndyekim@gmail.com>
2019-09-27 16:48:51 +0100

    fix(bug-fix): fix bug in algorthm to calculate transaction churn

--- src/components/WeekDaysLabel.jsx
+++ src/components/WeekDaysLabel.jsx
@@ -24,9 +24,11 @@ function getWeeksLabel(props) {
     for (let dayInWeek = firstYearDate.getDay() - 1, totalDays = 0; totalDays < 7; dayInWeek++ , totalDays++) {
         if (dayInWeek === WeekComponent.MAX_WEEK_DAYS) {
             dayInWeek = 0
+           // console.log(dayAbbr(dayInWeek))
             dayLabel.push(<li key={dayInWeek}>{dayAbbr(dayInWeek)}</li>)
         } else {
             dayLabel.push(<li key={dayInWeek}>{dayAbbr(dayInWeek)}</li>)
+           // console.log(dayAbbr(dayInWeek))
         }
     }
     return (
