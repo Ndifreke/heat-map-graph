962327d78f4f368928d1dd9015ed012daeac3094
ndifreke <ndyekim@gmail.com>
2019-09-28 22:45:38 +0100

    chore(performance): improve the algorithm for calculating the day informartion - add more test for day component

--- src/__tests__/DayComponent.test.jsx
+++ src/__tests__/DayComponent.test.jsx
@@ -7,7 +7,7 @@ import DayComponent, { COLOR_RANGE } from '../components/DayComponent'
 
 describe("Day Component", () => {
 
-    const props = { credit: 3, debit: 1, date: new Date() }
+    const props = { dailyAmounts: { "2019-03-31": 700 }, peakTransaction: 500, date: "2019-03-31" }
     it("it should render without errors", () => {
         const div = document.createElement('div');
         ReactDOM.render(<DayComponent {...props} />, div);
@@ -18,31 +18,41 @@ describe("Day Component", () => {
         const daycomponent = shallow(<DayComponent {...props} />)
         const wrapper = daycomponent.find("li")
         expect(wrapper.props().title).toBe(
-            props.date.toDateString()
-                .concat(` $${(props.credit + props.debit).toFixed(2)}`)
+            new Date(props.date).toDateString()
+                .concat(` $${(700).toFixed(2)}`)
         )
     })
 
     it("should determine the color for lower churn", () => {
-        // const daycomponent = mount(<DayComponent {...props} />)
-        // const wrapper = daycomponent.find("li")
-        // const { style } = wrapper.props().children.props
-        // expect(style.background).toBe(COLOR_RANGE.low.style.background) 
+        const dailyAmounts = { dailyAmounts: { "2019-03-31": 10 } } 
+        const daycomponent = mount(<DayComponent {...Object.assign({}, props, dailyAmounts)} />)
+        const dayWrapper = daycomponent.find("li")
+        const { style } = dayWrapper.props().children.props
+        expect(style.background).toBe(COLOR_RANGE.low.style.background)
     })
 
-    it("should determine the color for average churn", () => {
-        // const props =  Object.assign(props, {})
-        // const daycomponent = mount(<DayComponent {...props} />)
-        // const wrapper = daycomponent.find("li")
-        // const { style } = wrapper.props().children.props
-        // expect(style.background).toBe("#A52A2A")
+    it("should determine the color for midLow churn", () => {
+        const dailyAmounts = { dailyAmounts: { "2019-03-31": 249 } } 
+        const daycomponent = mount(<DayComponent {...Object.assign({}, props, dailyAmounts)} />)
+        const dayWrapper = daycomponent.find("li")
+        const { style } = dayWrapper.props().children.props
+        expect(style.background).toBe(COLOR_RANGE.midLow.style.background)
     })
 
-    it("should determine the color for Higher churn", () => {
-        // const props =  Object.assign(props, {})
-        // const daycomponent = mount(<DayComponent {...props} />)
-        // const wrapper = daycomponent.find("li")
-        // const { style } = wrapper.props().children.props
-        // expect(style.background).toBe("#A52A2A")
+    it("should determine the color for midHigh churn", () => {
+        const dailyAmounts = { dailyAmounts: { "2019-03-31": 300 } }  
+        const daycomponent = mount(<DayComponent {...Object.assign({}, props, dailyAmounts)} />)
+        const dayWrapper = daycomponent.find("li")
+        const { style } = dayWrapper.props().children.props
+        expect(style.background).toBe(COLOR_RANGE.midHigh.style.background)
     })
+
+        it("should determine the color for high churn", () => {
+            const dailyAmounts = { dailyAmounts: { "2019-03-31": 450 } }  
+            const daycomponent = mount(<DayComponent {...Object.assign({}, props, dailyAmounts)} />)
+            const dayWrapper = daycomponent.find("li")
+            const { style } = dayWrapper.props().children.props
+            expect(style.background).toBe(COLOR_RANGE.high.style.background)
+        })
+    
 })
